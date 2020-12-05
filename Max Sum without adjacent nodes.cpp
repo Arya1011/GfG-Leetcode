25ce557707c7144490ec9fe2cbd186a13d3cc37e
Aryamaan <54039704+Arya1011@users.noreply.github.com>
2020-12-05 11:42:29 +0530

    Create Max Sum without adjacent nodes.cpp

---
+++ Max Sum without adjacent nodes.cpp
@@ -0,0 +1,39 @@
+//https://www.geeksforgeeks.org/maximum-sum-nodes-binary-tree-no-two-adjacent/
+
+class Solution {
+public:
+    
+    //Memoize the value obtained for each node being considered as the root node in a map
+    map<int,int>mp;
+    
+    int helper(TreeNode*root){
+        
+        if(root==NULL){
+            return 0;        
+        }
+        
+        if(mp.find(root->val)!=mp.end()){
+            return mp[root->val];
+        }
+        
+        int left=0,right=0;
+        //Parent is chosen
+        if(root->left!=NULL){
+            left=helper(root->left->left)+helper(root->left->right);
+        }
+        
+        if(root->right!=NULL){
+            right=helper(root->right->right)+helper(root->right->left);
+        }
+        
+        return mp[root->val]=max(left+right+root->val,helper(root->left)+helper(root->right));    
+    }
+    
+    
+    int rob(TreeNode* root) {
+        if(!root)return 0;
+        int ans=helper(root);
+        return ans;
+        
+    }
+};
