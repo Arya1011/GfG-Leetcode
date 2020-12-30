0527820d93a813281d49930d878a80d8578ca6a1
Aryamaan <54039704+Arya1011@users.noreply.github.com>
2020-12-30 18:26:33 +0530

    Create Peak Element.cpp

---
+++ Peak Element.cpp
@@ -0,0 +1,34 @@
+
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        
+        int n=nums.size();
+        
+        int s=0;
+        int e=n-1;
+        
+        while(s<e){
+            
+            int mid=(s+e)/2;
+            
+            if(s==e){
+                return s;
+            }
+            
+            
+             if(nums[mid]>nums[mid+1]){
+                e=mid;
+            }
+            
+            else{
+                s=mid+1;
+            }
+            
+        }
+        
+        return s;
+        
+    }
+};
