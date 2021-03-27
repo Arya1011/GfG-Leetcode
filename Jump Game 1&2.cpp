9f07d8e37e4f4dbd8fb0cb5026c7d53cf8b0d660
Aryamaan <54039704+Arya1011@users.noreply.github.com>
2021-03-27 17:50:44 +0530

    Update Jump Game 1&2.cpp

--- Jump Game 1&2.cpp
+++ Jump Game 1&2.cpp
@@ -61,7 +61,6 @@ public:
 //Jump Game 2
 
 //DP appraoch O(n^2)
-
 class Solution {
 public:
     int jump(vector<int>& nums) 
@@ -90,4 +89,43 @@ public:
     }
 };
 
+
+//Greedy Solution O(n)
+class Solution{
+  public:
+    int minJumps(int arr[], int n){
+        // Your code here
+        
+        if(n==1){
+            return 0;
+        }
+        
+        if(n==0 || arr[0]==0){
+            return -1;
+        }
+        
+         int steps=arr[0];
+         int max_reach=arr[0];
+         int jumps=1;
+         for(int i=1;i<n;i++){
+             if(i==n-1){
+                 return jumps;
+             }
+             //check if we have a greater rwach found till now
+             max_reach=max(max_reach,i+arr[i]);
+             steps--;
+             if(steps==0)
+             {
+                 //that means we could have reached till here only
+                 if(i>=max_reach)
+                 {
+                     return -1;
+                 }
+                 jumps++;
+                 steps=max_reach-i;
+             }
+         }
+        return jumps;
+    }
+};
 
