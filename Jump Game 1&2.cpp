e3e674bfdfc30ab8b37118b32252a3a1b0ca771f
Aryamaan <54039704+Arya1011@users.noreply.github.com>
2021-03-27 16:34:35 +0530

    Update Jump Game 1&2.cpp

--- Jump Game 1&2.cpp
+++ Jump Game 1&2.cpp
@@ -1,33 +1,26 @@
 //https://leetcode.com/problems/jump-game/
 
+
+
+
+//Greedy Approach
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
-        int jumps=0;
-        if(n==1){
-            //zero jumps
+        
+        if(nums.empty()){
             return true;
         }
         
-        //this is my start and end
-        pair<int,int> intervals{0,0};
+        int lastPos=nums.size()-1;
         
-        for(int i=0;i<n;i++){
-            int can_reach=-1;
-            jumps++;
-            for(int j=intervals.first;j<=intervals.second;j++){
-                can_reach=max(can_reach,j+nums[j]);
-            }
-            if(can_reach>=n-1){
-                return true;
-            }
-            //update the starting point now
-            intervals={intervals.second+1,can_reach};
-            if(intervals.first>intervals.second){
-                 return false;
+        for(int i=nums.size()-1;i>=0;i--){
+            if(nums[i]+i>=lastPos){
+                 lastPos=i;
             }
         }
-        return false;
+        
+        return lastPos==0;
+        
     }
 };
