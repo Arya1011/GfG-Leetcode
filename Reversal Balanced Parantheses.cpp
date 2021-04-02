902b0e556aa1c77ba028872bf33041fbebcb309d
Aryamaan <54039704+Arya1011@users.noreply.github.com>
2021-04-02 20:43:51 +0530

    Create Reversal Balanced Parantheses.cpp

---
+++ Reversal Balanced Parantheses.cpp
@@ -0,0 +1,51 @@
+void solve(){
+    
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        stack<char> st;
+        int i=0;
+        int n=s.size();
+        int open=0;
+        int closed=0;
+        while(i<n){
+            
+            if(s[i]=='{'){
+                st.push(s[i]);
+            }
+            
+            if(s[i]=='}'){
+                if(st.empty()){
+                    closed++;
+                }
+                
+                else{
+                    st.pop();
+                }
+            }
+            
+            i++;
+        }
+        
+        while(!st.empty()){
+            open++;
+            st.pop();
+        }
+        
+        //cout<<open<<" "<<closed<<'\n';
+        
+        if((open+closed)%2==1){
+            cout<<-1<<endl;
+        }
+        
+        else{
+             cout<<ceil(float(open)/2.0)+ceil(float(closed)/2.0)<<endl;
+        }
+       
+        
+    }
+}
