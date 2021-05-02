0e1f1f2033d04441b8aff220a8f82e4d0687de43
Aryamaan <54039704+Arya1011@users.noreply.github.com>
2021-05-02 22:16:26 +0530

    Create K Swaps For max number.cpp

---
+++ K Swaps For max number.cpp
@@ -0,0 +1,47 @@
+
+public:
+    void helper(string s,string &mx,int k,int idx)
+    {
+        //swaps over
+        if(k==0){
+            return;
+        }
+        int n=s.length();
+        
+        
+        //precompute the next greater character
+        
+        char max_curr=s[idx];
+        
+        for(int i=idx+1;i<n;i++){
+            if(s[i]>max_curr){
+                max_curr=s[i];
+            }
+        }
+        
+        if(max_curr!=s[idx]){
+            --k;
+        }
+        
+        
+            for(int j=idx;j<n;j++){
+                if(s[j]==max_curr){
+                    swap(s[idx],s[j]);
+                    if(s.compare(mx)>0){
+                          mx=s;
+                    }
+                      helper(s,mx,k,idx+1);
+                      //backtrack
+                      swap(s[idx],s[j]);
+            }
+        }
+    }
+    
+    //Function to find the largest number after k swaps.
+    string findMaximumNum(string str, int k)
+    {
+       // code here.
+        string mx=str;
+        helper(str,mx,k,0);
+        return mx;
+    }
