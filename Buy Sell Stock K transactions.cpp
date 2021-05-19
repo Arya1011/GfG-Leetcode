e06a71d12f0bed32781bc64e621841afc4fc301c
Aryamaan <54039704+Arya1011@users.noreply.github.com>
2021-05-19 21:44:04 +0530

    Update Buy Sell Stock K transactions.cpp

--- Buy Sell Stock K transactions.cpp
+++ Buy Sell Stock K transactions.cpp
@@ -1,5 +1,6 @@
 
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
+//O(n^2 *K)
 class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
@@ -37,5 +38,49 @@ public:
         }
         
         return profit[k][n-1];
+    }
+};
+
+//O(nk)
+class Solution {
+  public:
+    int maxProfit(int K, int N, int A[]) {
+        // code here
+        
+        if(N==0 || K==0 || N==1){
+            return 0;
+        }
+        
+        if(N==2){
+            if(A[0]<A[1]){
+                return A[1]-A[0];
+                
+            }
+            
+            else{
+                return 0;
+            }
+        }
+        
+        vector<vector<int>> dp(K+1,vector<int>(N+1,0));
+        
+        
+        for(int i=0;i<=K;i++){
+            dp[i][0]=0;
+        }
+        
+        for(int i=0;i<=N;i++){
+            dp[0][i]=0;
+        }        
+        
+        
+        for(int i=1;i<=K;i++){
+            int prev_diff=INT_MIN;
+            for(int j=1;j<N;j++){
+                prev_diff=max(prev_diff,dp[i-1][j-1]-A[j-1]);
+                dp[i][j]=max(dp[i][j-1],prev_diff+A[j]);
+            }
+        }
+        return dp[K][N-1];
     }
 };
