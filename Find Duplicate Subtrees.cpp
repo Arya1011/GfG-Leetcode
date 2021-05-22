03b48fee642aa4d020303b6af56102c9f7f0e8d5
Aryamaan <54039704+Arya1011@users.noreply.github.com>
2021-05-22 22:55:16 +0530

    Create Find Duplicate Subtrees.cpp

---
+++ Find Duplicate Subtrees.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    map<string,int>mp;
+    vector<TreeNode*> ans;
+        
+    string helper(TreeNode*root){
+        if(root==NULL){
+            return "$";
+        }
+        
+        //Encode the subtree as a string
+        string s=to_string(root->val)+","+helper(root->left)+","+helper(root->right);
+        if(mp[s]==1){
+            ans.push_back(root);
+        }
+        
+        //Include the string in the map
+        mp[s]++;
+        
+        return s;
+        
+        
+    }
+    
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        
+           helper(root);
+        
+           return ans;
+        
+    }
+};
