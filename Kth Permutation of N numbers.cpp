090e911e1ca7a38b5777468e5db188a9271b1543
Aryamaan <54039704+Arya1011@users.noreply.github.com>
2021-05-29 16:45:05 +0530

    Create Kth Permutation of N numbers.cpp

---
+++ Kth Permutation of N numbers.cpp
@@ -0,0 +1,33 @@
+//https://leetcode.com/problems/permutation-sequence/
+class Solution {
+public:
+    string getPermutation(int n, int k) 
+    {
+        string ans="";
+        int fact=1;
+        vector<int> nums;
+        for(int i=1;i<n;i++)
+        {
+            nums.push_back(i);
+            fact=fact*i;
+        }
+        nums.push_back(n);
+        k=k-1;
+        while(true)
+        {
+             ans=ans+to_string(nums[k/fact]);
+             nums.erase(nums.begin()+(k/fact));
+             if(nums.size()==0){
+                 break;
+             }
+            
+             k=k%fact;
+             fact=fact/nums.size();
+        }
+        
+        
+        return ans;
+        
+        
+    }
+};
